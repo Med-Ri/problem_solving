5d202e6083c72f79398e0d2afdec2a4e41c1956b
M-Ridha <82166718+M-Ridha@users.noreply.github.com>
2024-02-11 15:58:16 +0100

    Sum_of_prime-indexed_elements

---
+++ Sum_of_prime-indexed_elements.js
@@ -0,0 +1,11 @@
+// ---------------------------------description-----------------------------------
+// In this Kata, you will be given an integer array and your task is to return the sum of elements occupying prime-numbered indices.
+
+// The first element of the array is at index 0.
+
+// Good luck!
+
+
+// -------------------------------------------------------------------------------
+
+
