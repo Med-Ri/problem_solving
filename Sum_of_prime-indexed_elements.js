b344d9303bbe1e94a4b8172bb2fa68d4c3fb42a3
M-Ridha <82166718+M-Ridha@users.noreply.github.com>
2024-02-11 16:06:17 +0100

    Sum_of_prime-indexed_elements

--- Sum_of_prime-indexed_elements.js
+++ Sum_of_prime-indexed_elements.js
@@ -7,7 +7,7 @@
 
 // -------------------------------------------------------------------------------
 
-function total(arr) {
+const total = (arr) =>  {
   let primeSum = 0;
   for (let i = 0; i < arr.length; i++) {
     let isPrime = true;
@@ -27,3 +27,6 @@ function total(arr) {
   }
   return primeSum;
 }
+
+
+console.log('result', total([0,1,2,3,4,5,6,7,8,9,10])) //17
