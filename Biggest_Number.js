524eab48f2419c7be41c4c3f2538884a10cda034
M-Ridha <82166718+M-Ridha@users.noreply.github.com>
2024-02-14 15:01:33 +0100

    Biggest_Number

--- Biggest_Number.js
+++ Biggest_Number.js
@@ -1,13 +1,32 @@
 // ---------------------------Description---------------------------------------
 
-// The task is to create a function called biggestNumber that takes an array of numbers as input and returns the largest number in the array. 
+// The task is to create a function called biggestNumber that takes an array of numbers as input and returns the largest number in the array.
 
-// ------------------------------------------------------------------
-
+// -----------------------------------------------------------------------------
 
 //Solution 1 using (ES6)
-const biggestNumber = (arr) => {
+const biggestNumber01 = (arr) => {
     return Math.max(...arr)
 }
 
-console.log('result', biggestNumber([19,1,0,20,2,105,-255,30,106])) //result ==> 106
+console.log('result_01', biggestNumber01([19,1,0,20,2,105,-255,30,106])) //result ==> 106
+
+
+
+//solution 2 using js
+const biggestNumber02 = (arr) => {
+  // Initialize max to the first element of the array
+  let max = arr[0];
+  // Loop through the array starting from the second element
+  for (let index = 1; index < arr.length; index++) {
+    // Compare each element to the current max
+    if (arr[index] > max) {
+      // If the current element is greater, update max
+      max = arr[index];
+    }
+  }
+  // Return the maximum value found
+  return max;
+};
+
+console.log('result_02', biggestNumber02([19,1,0,20,2,105,-255,30,106])) //result ==> 106
