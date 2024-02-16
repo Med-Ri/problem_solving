55cbff3596c47fc3a6db0c88ed44216e735773fa
M-Ridha <82166718+M-Ridha@users.noreply.github.com>
2024-02-16 19:54:00 +0100

    Days_in_the_year

--- Days_in_the_year.js
+++ Days_in_the_year.js
@@ -16,3 +16,13 @@
 // So the years 0, -64 and 2016 will return 366 days. Whilst 1974, -10 and 666 will return 365 days.
 
 // ---------------------------------------------------------------------------
+
+
+function yearDays(year)
+{
+   if ((year % 4 === 0 && year % 100 !== 0) || (year % 400 === 0)) {
+        return year + " has 366 days";
+    } else {
+        return year + " has 365 days";
+    }
+}
