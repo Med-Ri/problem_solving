0b72d7fac84f302c73654704ab937d52f1aafec2
M-Ridha <82166718+M-Ridha@users.noreply.github.com>
2024-03-03 21:52:33 +0100

    Array - squareUp_b!

--- Array - squareUp_b!.js
+++ Array - squareUp_b!.js
@@ -10,7 +10,7 @@
 // ---------------------------------------------------------------------------
 
 
-function squareUp(n) {
+const squareUp = (n) => {
     let result = [];
     for (let i = n; i > 0; i--) {
       for (let j = 1; j <= n; j++) {
