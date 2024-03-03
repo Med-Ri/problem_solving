0b31c9c61fdf800b35f243393b9e02e07d7a8535
M-Ridha <82166718+M-Ridha@users.noreply.github.com>
2024-03-03 21:52:06 +0100

    Array - squareUp_b!

--- Array - squareUp_b!.js
+++ Array - squareUp_b!.js
@@ -8,3 +8,18 @@
 // squareUp(4) => [0, 0, 0, 1, 0, 0, 2, 1, 0, 3, 2, 1, 4, 3, 2, 1]
 // 0 <= n <= 1000.
 // ---------------------------------------------------------------------------
+
+
+function squareUp(n) {
+    let result = [];
+    for (let i = n; i > 0; i--) {
+      for (let j = 1; j <= n; j++) {
+        if (j <= i) {
+          result.unshift(j);
+        } else {
+          result.unshift(0);
+        }
+      }
+    }
+    return result;
+  }
