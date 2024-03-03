346ab195ade952e67c5c65335b933c3fff0e7055
M-Ridha <82166718+M-Ridha@users.noreply.github.com>
2024-03-03 21:51:01 +0100

    Array - squareUp_b!

---
+++ Array - squareUp_b!.js
@@ -0,0 +1,10 @@
+// -------------------------------Description---------------------------------
+// This is a question from codingbat
+
+// Given an integer n greater than or equal to 0, create and return an array with the following pattern:
+
+// squareUp(3) => [0, 0, 1, 0, 2, 1, 3, 2, 1]
+// squareUp(2) => [0, 1, 2, 1]
+// squareUp(4) => [0, 0, 0, 1, 0, 0, 2, 1, 0, 3, 2, 1, 4, 3, 2, 1]
+// 0 <= n <= 1000.
+// ---------------------------------------------------------------------------
