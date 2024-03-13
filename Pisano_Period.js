c38cfed58e2d86573970b85736ce618f476e537e
M-Ridha <82166718+M-Ridha@users.noreply.github.com>
2024-03-13 13:14:28 +0100

    Pisano_Period

--- Pisano_Period.js
+++ Pisano_Period.js
@@ -5,7 +5,7 @@
 
 // Defined by the recurrence relation
 
-//   Fib(0) = 0 
+//   Fib(0) = 0
 //   Fib(1) = 1
 //   Fib(2) = 1
 //   Fib(i) = Fib(i-1) + Fib(i-2)
@@ -13,26 +13,24 @@
 
 // For example, the sequence of Fibonacci numbers modulo 3 begins:
 
-// 0, 1, 1, 2, 0, 2, 2, 1, 0, 1, 1, 2, 0, 2, 2, 1, 0, 1, 1, 2, 0, 2, 2, 1, ... 
+// 0, 1, 1, 2, 0, 2, 2, 1, 0, 1, 1, 2, 0, 2, 2, 1, 0, 1, 1, 2, 0, 2, 2, 1, ...
 // This sequence has period 8. The repeating patter is 0, 1, 1, 2, 0, 2, 2, 1 So Pisano(3) = 8
 
 // Your task is to write the Pisano function that takes an integer n and outputs the length of pisano period.
 // ------------------------------------------------------------------------------------
 
+const pisano = (n) => {
+  let previous = 0;
+  let current = 1;
+  let period = 0;
 
-
-function pisano(n){
-    let previous = 0;
-     let current = 1;
-     let period = 0;
- 
-     for (let i = 0; i < n * n; i++) {
-         let temp = (previous + current) % n;
-         previous = current;
-         current = temp;
-         period++;
-         if (previous === 0 && current === 1) {
-             return period;
-         }
-     } 
- }
+  for (let i = 0; i < n * n; i++) {
+    let temp = (previous + current) % n;
+    previous = current;
+    current = temp;
+    period++;
+    if (previous === 0 && current === 1) {
+      return period;
+    }
+  }
+}
