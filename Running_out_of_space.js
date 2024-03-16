a6458124d707c7c2189ed197937e472ebe23533c
M-Ridha <82166718+M-Ridha@users.noreply.github.com>
2024-03-16 23:38:56 +0100

    Running_out_of_space

--- Running_out_of_space.js
+++ Running_out_of_space.js
@@ -3,14 +3,25 @@
 // For example, running this function on the array ['i', 'have','no','space'] would produce ['i','ihave','ihaveno','ihavenospace']
 // ------------------------------------------------------------------------------------
 
-
-
-function spacey(array){
-    let result = [];
-      let accumulatedWord = '';
-      for (let word of array) {
-          accumulatedWord += word.replace(/\s/g, '');
-          result.push(accumulatedWord);
-      }
-      return result;
+const spacey = (array) => {
+  let result = [];
+  let accumulatedWord = "";
+  for (let word of array) {
+    accumulatedWord += word.replace(/\s/g, "");
+    result.push(accumulatedWord);
   }
+  return result;
+};
+
+
+
+
+let inputArray1 = ['kevin', 'has', 'no', 'space'];
+let outputArray1 = spacey(inputArray1);
+console.log('result', outputArray1) // => result [ 'kevin', 'kevinhas', 'kevinhasno', 'kevinhasnospace' ]
+
+// --------------------------------------------------
+
+let inputArray2 = ['this', 'cheese', 'has', 'no', 'holes'];
+let outputArray2 = spacey(inputArray2);
+console.log(outputArray2); // => ['this','thischeese','thischeesehas','thischeesehasno','thischeesehasnoholes']
