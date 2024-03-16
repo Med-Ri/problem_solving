c15ad7b6a042cb30f42e187752f2c0d7fffc89ce
M-Ridha <82166718+M-Ridha@users.noreply.github.com>
2024-03-16 23:34:56 +0100

    Running_out_of_space

--- Running_out_of_space.js
+++ Running_out_of_space.js
@@ -2,3 +2,15 @@
 // Kevin is noticing his space run out! Write a function that removes the spaces from the values and returns an array showing the space decreasing.
 // For example, running this function on the array ['i', 'have','no','space'] would produce ['i','ihave','ihaveno','ihavenospace']
 // ------------------------------------------------------------------------------------
+
+
+
+function spacey(array){
+    let result = [];
+      let accumulatedWord = '';
+      for (let word of array) {
+          accumulatedWord += word.replace(/\s/g, '');
+          result.push(accumulatedWord);
+      }
+      return result;
+  }
