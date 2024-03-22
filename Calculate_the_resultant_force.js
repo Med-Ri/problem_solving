8b6afad6d664124cd5ae7db5956383bf8414cbaa
M-Ridha <82166718+M-Ridha@users.noreply.github.com>
2024-03-22 12:45:49 +0100

    Calculate_the_resultant_force

--- Calculate_the_resultant_force.js
+++ Calculate_the_resultant_force.js
@@ -24,4 +24,25 @@
 
 // ------------------------------------------------------------------------------------------------
 
+const solution = (force1, force2, theta) => {
+  // Convert angle from degrees to radians
+  var thetaRad = (theta * Math.PI) / 180;
 
+  // Calculate the components of force2
+  var force2x = force2 * Math.cos(thetaRad);
+  var force2y = force2 * Math.sin(thetaRad);
+
+  // Calculate the components of R
+  var Rx = force1 + force2x;
+  var Ry = force2y;
+
+  // Calculate the magnitude of R
+  var R = Math.sqrt(Rx * Rx + Ry * Ry);
+
+  // Calculate the angle phi (angle between R and F1)
+  var phiRad = Math.atan2(Ry, Rx);
+  var phi = (phiRad * 180) / Math.PI;
+
+  // Return the resultant force R and angle phi
+  return [R, phi];
+};
