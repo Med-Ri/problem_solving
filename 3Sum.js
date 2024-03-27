54af9d971e9b4bb377c8922dcee1f55871722d16
M-Ridha <82166718+M-Ridha@users.noreply.github.com>
2024-03-27 15:04:45 +0100

    3Sum

--- 3Sum.js
+++ 3Sum.js
@@ -30,3 +30,46 @@
 // -105 <= nums[i] <= 105
 
 // -----------------------------------------------------------------------------------------------
+
+
+
+
+function threeSum(nums) {
+    nums.sort((a, b) => a - b);
+    const result = [];
+    const n = nums.length;
+
+    for (let i = 0; i < n - 2; i++) {
+        // Skip duplicate values of nums[i]
+        if (i > 0 && nums[i] === nums[i - 1]) {
+            continue;
+        }
+
+        let left = i + 1;
+        let right = n - 1;
+
+        while (left < right) {
+            const total = nums[i] + nums[left] + nums[right];
+            if (total === 0) {
+                result.push([nums[i], nums[left], nums[right]]);
+
+                // Skip duplicate values of nums[left] and nums[right]
+                while (left < right && nums[left] === nums[left + 1]) {
+                    left++;
+                }
+                while (left < right && nums[right] === nums[right - 1]) {
+                    right--;
+                }
+
+                left++;
+                right--;
+            } else if (total < 0) {
+                left++;
+            } else {
+                right--;
+            }
+        }
+    }
+
+    return result;
+}
