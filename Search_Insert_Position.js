b5596406215ae53a75bd4a5702cf2caa459a51b7
M-Ridha <82166718+M-Ridha@users.noreply.github.com>
2024-04-04 22:19:48 +0100

    Search_Insert_Position

--- Search_Insert_Position.js
+++ Search_Insert_Position.js
@@ -3,7 +3,6 @@
 // Given a sorted array of distinct integers and a target value, return the index if the target is found. If not, return the index where it would be if it were inserted in order.
 // You must write an algorithm with O(log n) runtime complexity.
 
-
 // Example 1:
 // Input: nums = [1,3,5,6], target = 5
 // Output: 2
@@ -15,7 +14,7 @@
 // Example 3:
 // Input: nums = [1,3,5,6], target = 7
 // Output: 4
- 
+
 // Constraints:
 // 1 <= nums.length <= 104
 // -104 <= nums[i] <= 104
@@ -24,21 +23,21 @@
 
 // ---------------------------------------------------------------------------------
 
-var searchInsert = function(nums, target) {
-    let low = 0;
-    let high = nums.length - 1;
-    
-    while (low <= high) {
-        const mid = Math.floor((low + high) / 2);
-        
-        if (nums[mid] === target) {
-            return mid;
-        } else if (nums[mid] < target) {
-            low = mid + 1;
-        } else {
-            high = mid - 1;
-        }
+const searchInsert = (nums, target) => {
+  let low = 0;
+  let high = nums.length - 1;
+
+  while (low <= high) {
+    const mid = Math.floor((low + high) / 2);
+
+    if (nums[mid] === target) {
+      return mid;
+    } else if (nums[mid] < target) {
+      low = mid + 1;
+    } else {
+      high = mid - 1;
     }
-    
-    return low;
+  }
+
+  return low;
 };
