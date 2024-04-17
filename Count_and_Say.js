8a29ad3e4191376a5b73473c2bfce8038531b2c7
M-Ridha <82166718+M-Ridha@users.noreply.github.com>
2024-04-17 19:43:41 +0100

    Count_and_Say

--- Count_and_Say.js
+++ Count_and_Say.js
@@ -26,3 +26,21 @@
 
 // -----------------------------------------------------------------------------------
 
+var countAndSay = function(n) {
+    if (n === 1) {
+       return "1";
+   } else {
+       let prev = countAndSay(n - 1);
+       let result = "";
+       let count = 1;
+       for (let i = 0; i < prev.length; i++) {
+           if (prev[i] === prev[i + 1]) {
+               count++;
+           } else {
+               result += count + prev[i];
+               count = 1;
+           }
+       }
+       return result;
+   }
+};
