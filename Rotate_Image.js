4623135c31972e135c9a3c6e6a24bbdcab945382
M-Ridha <82166718+M-Ridha@users.noreply.github.com>
2024-04-26 15:54:39 +0100

    Rotate_Image

--- Rotate_Image.js
+++ Rotate_Image.js
@@ -43,3 +43,27 @@ const rotate = (matrix) => {
     }
   }
 };
+
+
+// Example usage:
+const matrix1 = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9]
+];
+
+const matrix2 = [
+    [5, 1, 9, 11],
+    [2, 4, 8, 10],
+    [13, 3, 6, 7],
+    [15, 14, 12, 16]
+];
+
+rotate(matrix1);
+rotate(matrix2);
+
+console.log("Rotated matrix 1:");
+matrix1.forEach(row => console.log(row));
+
+console.log("Rotated matrix 2:");
+matrix2.forEach(row => console.log(row));
