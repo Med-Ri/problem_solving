2e99f9b5eac7ad8ddb512dbcbbbecaa9edf53f2c
M-Ridha <82166718+M-Ridha@users.noreply.github.com>
2024-05-21 22:01:09 +0100

    Length_of_Last_Word

--- Length_of_Last_Word.js
+++ Length_of_Last_Word.js
@@ -39,3 +39,8 @@ const lengthOfLastWord = (s) => {
 
 
 
+
+// Example usage:
+console.log(lengthOfLastWord("Hello World")); // Output: 5
+console.log(lengthOfLastWord("   fly me   to   the moon  ")); // Output: 4
+console.log(lengthOfLastWord("luffy is still joyboy")); // Output: 6
