1e2e1e28b3159499352c1dcd6a07e786f408875b
M-Ridha <82166718+M-Ridha@users.noreply.github.com>
2024-07-07 20:29:56 +0100

    FindTheDivisors

--- FindTheDivisors.js
+++ FindTheDivisors.js
@@ -21,3 +21,10 @@ const divisors = (integer) => {
 
   return divs.length ? divs : integer + " is prime";
 }
+
+
+
+
+console.log(divisors(12)) // should return [2,3,4,6]
+console.log(divisors(25)) // should return [5]
+console.log(divisors(13)) // should return "13 is prime"
