0d8d73e54d784bde32065ade26e1646ff9bc62a6
M-Ridha <82166718+M-Ridha@users.noreply.github.com>
2024-07-09 13:51:35 +0100

    FindTheUniqueNumber

--- FindTheUniqueNumber.js
+++ FindTheUniqueNumber.js
@@ -18,3 +18,8 @@ const findUniq = (arr) => {
     // Find the unique number by filtering numbers that occur only once
     return arr.find(num => arr.indexOf(num) === arr.lastIndexOf(num));
 }
+
+
+// Example usage:
+console.log(findUniq([1, 1, 1, 2, 1, 1])); // Output: 2
+console.log(findUniq([0, 0, 0.55, 0, 0])); // Output: 0.55
