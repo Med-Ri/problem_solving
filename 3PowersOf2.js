ed0cc2f6f83ecfd741b6add859e418f6d5c9b27b
M-Ridha <82166718+M-Ridha@users.noreply.github.com>
2024-07-12 22:58:45 +0100

    3PowersOf2

---
+++ 3PowersOf2.js
@@ -0,0 +1,16 @@
+// -----------------------------------------Description-------------------------------------------------------------------
+
+// Write a function that accepts a number, and checks it can be represented as a sum of exactly 3 powers of 2. (n == 2**i + 2**j + 2**k, i, j, k >= 0)
+
+// For example:
+// three_powers(2)  # False
+// three_powers(3)  # True, 3 = 2**0 + 2**0 + 2**0
+// three_powers(5)  # True, 5 = 2**0 + 2**1 + 2**1
+// three_powers(15)  # False
+
+// Input
+// 1 <= n <= 2 ** 512 - 1
+
+// There are 100 performance tests in languages with bigints, and a huge amount in languages without.
+
+// -----------------------------------------------------------------------------------------------------------------------
